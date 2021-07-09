b094dc14080c1d99bc30996fa835ebf3cbb8190b
Cerallin <cerallin@cerallin.top>
2021-07-09 13:45:40 +0800

    Add novel-format.js
    Signed-off-by: Cerallin <cerallin@cerallin.top>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # masiro-scripts
 真白萌网站油猴脚本
+
+## 说明
+
+[novel-format](./novel-format.js) 针对阅读样式进行优化，取消白底，修改字体，修改图片宽度。
+
+## 安装
+请了解油猴脚本的安装方法。
+
+## LICENSE
+
+GPL3.0+
---
+++ novel-format.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         真白萌阅读样式优化
+// @namespace    http://tampermonkey.net/
+// @version      0.9
+// @description  修改真白萌阅读样式
+// @author       Cerallin <cerallin@cerallin.top>
+// @match        https://masiro.me/admin/novelReading*
+// @icon         https://www.google.com/s2/favicons?domain=masiro.me
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Your code here...
+  const html = document.getElementsByTagName('html')[0];
+  const style = document.createElement("style");
+  style.innerHTML = `
+@font-face {
+    font-family: 'Noto Serif';
+    src: url(https://blog-cdn.cerallin.top/fonts/NotoSerifSC/NotoSerifSC-Regular.otf) format('opentype');
+    font-weight: 400;
+    font-size: 28px;
+    font-style: normal
+}
+.box-body p, .box-body span {
+    line-height: 1.5em;
+    text-align: justify;
+    font-family: 'Noto Serif', serif;
+}
+
+.box-body.nvl-content img {
+    width: 80% !important;
+    margin-top: .6em;
+    margin-bottom: .6em;
+}
+`;
+  html.appendChild(style);
+
+  let node = $('.box-body.nvl-content')[0];
+  let stack = [node];
+  // DFS
+  while (node = stack.pop()) {
+    stack = stack.concat(Array.prototype.slice.call(node.children))
+    let cur = $(node);
+    cur.removeAttr('style');
+    if (cur.is('br') && cur.prev().is('br') && cur.prev().prev().is('br'))
+      cur.remove();
+  }
+})();
